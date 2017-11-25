5d0849e2ccff18b728220c73a941ef87ddfac9c4
selfbot1 <33973629+selfbot1@users.noreply.github.com>
2017-11-25 10:59:24 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'help'= {
+      message.reply('Help is not done yet but it will be soon!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
